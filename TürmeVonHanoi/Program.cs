9da7d9520d0b2e1feb15594698ef734a50a18f28
Hex.Official <jan-heiner@web.de>
2024-10-06 17:52:16 +0200

    Projektdateien hinzufügen.

---
+++ TürmeVonHanoi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TürmeVonHanoi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lösungsalgorithmus für die Türme von Hanoi\n" +
+                               "Wie viele Spielsteine gibt es?");
+
+            if (int.TryParse(Console.ReadLine(), out int spielsteine))
+            {
+                TürmeVerschieben(spielsteine, 'L', 'M', 'R');
+            }
+
+            Console.ReadLine();
+        }
+
+        static void TürmeVerschieben(int spielsteinZuVerschieben, char von, char über, char nach)
+        {
+            if (spielsteinZuVerschieben > 0)
+            {
+                TürmeVerschieben(spielsteinZuVerschieben - 1, von, nach, über);
+                Console.WriteLine($"Schiebe {spielsteinZuVerschieben} von {von} nach {nach}");
+                TürmeVerschieben(spielsteinZuVerschieben - 1, über, von, nach);
+            }
+        }
+    }
+}
